7119d130be0815825475a484214fafcb80f8a73c
Manik-Mittal <manikmittal2003@gmail.com>
2025-07-26 17:53:32 +0530

    created transaction ms

---
+++ transaction/src/test/java/com/finverse/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finverse.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
