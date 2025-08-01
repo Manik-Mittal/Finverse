7b30b814c336e5fe6df78fb6d576e3e51f652ac0
Manik-Mittal <manikmittal2003@gmail.com>
2025-08-02 00:35:13 +0530

    added account service

---
+++ accountservice/src/main/java/com/accountservice/dto/BalanceUpdateRequest.java
@@ -0,0 +1,32 @@
+package com.accountservice.dto;
+
+public class BalanceUpdateRequest {
+    private String accountNumber;
+    private double amount;
+    private String transactionType;
+
+    // Getters and Setters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+}
