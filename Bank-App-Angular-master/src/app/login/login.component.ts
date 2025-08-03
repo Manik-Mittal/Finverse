555a9701fc039444ac6f5e7a546c32747fc5a1de
Ganpat Singh <ganpat.singh@oracle.com>
2025-08-03 10:30:08 +0530

    added Banking frontend with angular

---
+++ Bank-App-Angular-master/src/app/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../Services/data.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  data1 = "Enter your username"
+  data2 = "Enter your password"
+  // acno:any
+  // passwd:any
+
+  constructor(private router: Router, private ds: DataService, private fb: FormBuilder) { }
+
+  loginForm = this.fb.group({
+    // acno: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+    // psw: ['', [Validators.required, Validators.pattern('[0-9a-zA-Z]+')]]
+    username: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+     password: [' ', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]]
+
+  })
+
+  login() {
+    console.log("hey guys")
+    var username = this.loginForm.value.username
+    var password = this.loginForm.value.password
+
+    if (this.loginForm.valid) {
+      this.ds.login(username,password).subscribe((result:any)=>{
+
+        localStorage.setItem("currentUser",result.currentUser)
+        localStorage.setItem("currentAcno",JSON.stringify(result.currentAcno))
+        localStorage.setItem("token",JSON.stringify(result.token))
+
+       // alert(result.messsage)
+        this.router.navigateByUrl("dashboard")
+      },
+      result=>{
+        //alert(result.error.messsage)
+      }
+      )
+    }
+    else {
+      alert('invalid form')
+    }
+
+  }
+
+
+}
+
+// ---------------------------------------
+
+
