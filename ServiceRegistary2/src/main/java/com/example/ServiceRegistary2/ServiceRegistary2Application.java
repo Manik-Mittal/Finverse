bace618a1108038a014371e3c888da734f72a44a
Ganpat Singh <ganpat.singh@oracle.com>
2025-08-03 17:23:38 +0530

    changes in eureka server

--- ServiceRegistary2/src/main/java/com/example/ServiceRegistary2/ServiceRegistary2Application.java
+++ ServiceRegistary2/src/main/java/com/example/ServiceRegistary2/ServiceRegistary2Application.java
@@ -2,11 +2,17 @@ package com.example.ServiceRegistary2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistary2Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServiceRegistary2Application.class, args);
 	}
 
